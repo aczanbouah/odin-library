b8a167856e16da56cab564ee2c07360f5862ae54
Adnan Cezar Zanbouah <cezarzanbouah@gmail.com>
2024-03-14 00:21:21 +0200

    Add functionality to construct a Book object using form input data

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const myLibrary = [];
+const submitBtn = document.querySelector("#submit-btn");
+const addNewBookBtn = document.querySelector("#add-new-book");
+const modal = document.querySelector(".modal");
+const bookForm = document.querySelector(".form");
 
-function Book(author, title, pages, read) {
-  this.author = author;
+function Book(title, author, pages, read) {
   this.title = title;
+  this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
-function addBookToLibrary(book) {
-  book = new Book("MJ", "Last dance", 69, true);
+function addBookToLibrary() {
+  const bookTitle = document.querySelector("#book-title");
+  const bookAuthor = document.querySelector("#book-author");
+  const bookPages = document.querySelector("#book-pages");
+  const isBookRead = document.querySelector("#book-read");
+  const book = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    isBookRead.value
+  );
   myLibrary.push(book);
-  return myLibrary;
 }
+// TODO: Make modal close after submit and clear the form inputs
+submitBtn.addEventListener("click", () => {
+  addBookToLibrary();
+  bookForm.reset();
+});
+addNewBookBtn.addEventListener("click", () => {
+  modal.showModal();
+});
